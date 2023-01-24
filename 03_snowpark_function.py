d1db9516f17a336d07c41d33d31cd0029a2da810
Rob Goretsky <robg@eventbrite.com>
2023-01-24 18:10:04 -0500

    workin

--- 03_snowpark_function.py
+++ 03_snowpark_function.py
@@ -1,44 +1,64 @@
 from utils import get_snowpark_session, get_snowflake_connection
-from snowflake.snowpark.functions import sum, year, udf
+from snowflake.snowpark.functions import call_udf, count, col
 
 # This will become a Snowflake UDF when we register it!
 def get_email_domain(email_address: str) -> str:
-    if "@" not in email_address:
+    if not email_address:
+        return ""
+    elif "@" not in email_address:
         return ""
     else:
-        email_address.split("@")[1]
+        return email_address.split("@")[1]
+
+
+def invoke_udf_with_sql():
+    print("Calling UDF via SQL to get Top 10 email domains")
+    cursor = get_snowflake_connection().cursor()
+    result = cursor.execute(
+        """
+        select
+          eb.get_email_domain(email) as email_domain,
+          count(*) as domain_count
+        from
+          eb.orders
+        where
+          year(created) = 2022
+        group by 1
+        order by 2 desc
+        limit 10
+    """
+    ).fetchall()
+    print(result)
+
+
+def invoke_udf_with_snowpark_client():
+    # Call it via Dataframe API
+    result = (
+        session.table("eb.orders")
+        .with_column("email_domain", call_udf("eb.get_email_domain", col("email")))
+        .where("year(created) = 2022")
+        .group_by("email_domain")
+        .agg(count("*").as_("domain_count"))
+        .sort(col("domain_count").desc())
+        .limit(10)
+    )
+    result.show()
 
 
 if __name__ == "__main__":
     session = get_snowpark_session()
 
     # Register the UDF
-    print("Registering UDF, eb.get_email_domain")
-    session.udf.register(
-        func=get_email_domain,
-        name="eb.get_email_domain",
-        session=session,
-        replace=True,
-        is_permanent=True,
-        stage_location="@DEV_UTIL.STAGES.S3_EXPORT_DATA",
-    )
-    print("Registered succesfully!")
+    # print("Registering UDF, eb.get_email_domain")
+    # session.udf.register(
+    #     func=get_email_domain,
+    #     name="eb.get_email_domain",
+    #     session=session,
+    #     replace=True,
+    #     is_permanent=True,
+    #     stage_location="@DEV_UTIL.STAGES.S3_EXPORT_DATA",
+    # )
+    # print("Registered succesfully!")
 
-    # Call it via SQL
-    print("Calling UDF via SQL to get Top 10 email domains")
-    cursor = get_snowflake_connection().cursor()
-    result = cursor.execute(
-        """
-        select 
-          eb.get_email_domain(email) as email_domain, 
-          count(*) as domain_count 
-        from 
-          eb.orders 
-        group by 1 
-        order by 2 desc 
-        limit 10
-    """
-    ).fetchall()
-    print(result)
-
-    # Call it via Dataframe API
+    # invoke_udf_with_sql()
+    invoke_udf_with_snowpark_client()
